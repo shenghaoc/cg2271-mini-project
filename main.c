d44e7837c3c45e6ba7ac0f49bd3be69eeae6050a
Chen Shenghao <chen.shenghao@u.nus.edu>
2020-03-28 10:28:44 +0800

    Add template initi code

--- main.c
+++ main.c
@@ -5,6 +5,100 @@
 #include "RTE_Components.h"
 #include  CMSIS_device_header
 #include "cmsis_os2.h"
+
+#define RED_LED 18 // PortB Pin 18
+#define GREEN_LED 19 // PortB Pin 19
+#define BLUE_LED 1 // PortD Pin 1
+#define MASK(x) (1 << (x))
+
+#define PTB0_Pin 0
+#define PTB1_Pin 1
+
+#define BAUD_RATE 9600
+#define UART_TX_PORTE22 22
+#define UART_RX_PORTE23 23
+#define UART2_INT_PRIO 128
+
+void initGPIO(void) {
+	// Enable Clock to PORTB and PORTD
+	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTD_MASK));
+	
+	// Configure MUX settings to make all 3 pins GPIO
+	
+	PORTB->PCR[RED_LED] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[RED_LED] |= PORT_PCR_MUX(1);
+	
+	PORTB->PCR[GREEN_LED] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[GREEN_LED] |= PORT_PCR_MUX(1);
+	
+	PORTD->PCR[BLUE_LED] &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[BLUE_LED] |= PORT_PCR_MUX(1);
+	
+	// Set Data Direction Registers for PortB and PortD
+	PTB->PDDR |= (MASK(RED_LED) | MASK(GREEN_LED));
+	PTD->PDDR |= MASK(BLUE_LED);
+}
+
+void initPWM(void){
+	
+	//Enable clock for Port B
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	
+	//Clear PTB0_Pin and PTB1_Pin settings and configure as PWM
+	PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
+	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
+	
+	//Enable clock for TPM1
+	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	
+	//Clear the TPMSRC bits and set the clock source as MCGFLLCLK clock or MCGPLLCLK/2  
+	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
+	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
+	
+	TPM1->MOD = 0;
+	TPM1_C0V = 0;
+	
+	//Increment on every LPTPM counter clock, set pre-scaling to 128 and operate in count up
+	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM1->SC |= (TPM_SC_CMOD(1)) | (TPM_SC_PS(7));
+	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	//Set as edge-aligned PWM with high true pulses
+	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+}
+
+void initUART2(uint32_t baud_rate)
+{
+	uint32_t divisor, bus_clock;
+	
+	SIM->SCGC4 |= SIM_SCGC4_UART2_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	
+	PORTE->PCR[UART_TX_PORTE22] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[UART_TX_PORTE22] |= PORT_PCR_MUX(4);
+	
+	PORTE->PCR[UART_RX_PORTE23] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[UART_RX_PORTE23] |= PORT_PCR_MUX(4);
+	
+	UART2->C2 &= ~((UART_C2_TE_MASK) | (UART_C2_RE_MASK));
+	
+	bus_clock = (DEFAULT_SYSTEM_CLOCK)/2;
+	divisor = bus_clock / (baud_rate * 16);
+	UART2->BDH = UART_BDH_SBR(divisor >> 8);
+	UART2->BDL = UART_BDL_SBR(divisor);
+	
+	UART2->C1 = 0;
+	UART2->S2 = 0;
+	UART2->C3 = 0;
+	
+	UART2->C2 |= ((UART_C2_TE_MASK) | (UART_C2_RE_MASK));
+}
+
+
+
  
 /*----------------------------------------------------------------------------
  * Application main thread
@@ -19,6 +113,9 @@ int main (void) {
  
   // System Initialization
   SystemCoreClockUpdate();
+	initGPIO();
+	initUART2(BAUD_RATE);
+	initPWM();
   // ...
  
   osKernelInitialize();                 // Initialize CMSIS-RTOS
